6e79ad3332edfdd62370abd48d5bb75fc27dac8d
GrayDiamond493 <47148399+GrayDiamond493@users.noreply.github.com>
2022-05-29 10:47:16 -0500

    added images

---
+++ lab6/README.md
@@ -0,0 +1,11 @@
+# Lab 6 - Unidad 2 - Big Data
+
+## Wordcount EMR
+
+## Woedcount Jupyter
+
+## Hive (Tablas, Queries)
+
+## Hive (Wordcount)
+
+## Jupyter SparkSQL
---
+++ lab6/scripts/wc-pyspark.py
@@ -0,0 +1,14 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("WordCount").getOrCreate()
+sc = spark.sparkContext
+# puede no necesitar instanciar las variables spark y sc si esta ejecutando en AWS EMR:
+
+files_rdd = sc.textFile("s3a://aagutierrldatalake/raw/gutenberg-small/*.txt")
+#files_rdd = sc.textFile("hdfs:///datasets/gutenberg-small/*.txt")
+wc_unsort = files_rdd.flatMap(lambda line: line.split()).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+wc = wc_unsort.sortBy(lambda a: -a[1])
+for tupla in wc.take(10):
+    print(tupla)
+wc.saveAsTextFile("hdfs:///tmp/wcout3")
+wc.coalesce(1).saveAsTextFile("hdfs:///tmp/wcout4")
